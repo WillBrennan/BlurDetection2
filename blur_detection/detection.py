824d9ad805d5f9286b51678160d5a708f4658e35
WillBrennan <WillBrennan@users.noreply.github.com>
2019-12-26 12:23:04 +0000

    blur_detection - cleanup image processing and stop divide by zero warning in numpy.log

--- blur_detection/detection.py
+++ blur_detection/detection.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import logging
-
 import cv2
 import numpy
 
 
-def fix_image_size(image, expected_pixels=2E6):
-    ratio = float(expected_pixels) / float(image.shape[0] * image.shape[1])
+def fix_image_size(image: numpy.array, expected_pixels: float = 2E6):
+    ratio = expected_pixels / (image.shape[0] * image.shape[1])
     return cv2.resize(image, (0, 0), fx=ratio, fy=ratio)
 
 
-def estimate_blur(image, threshold=100):
+def estimate_blur(image: numpy.array, threshold: int = 100):
     if image.ndim == 3:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
@@ -20,7 +18,10 @@ def estimate_blur(image, threshold=100):
     return blur_map, score, bool(score < threshold)
 
 
-def pretty_blur_map(blur_map, sigma=5):
-    abs_image = numpy.log(numpy.abs(blur_map).astype(numpy.float32))
+def pretty_blur_map(blur_map: numpy.array, sigma: int = 5, min_abs: float = 0.5):
+    abs_image = numpy.abs(blur_map).astype(numpy.float32)
+    abs_image[abs_image < min_abs] = min_abs
+
+    abs_image = numpy.log(abs_image)
     cv2.blur(abs_image, (sigma, sigma))
     return cv2.medianBlur(abs_image, sigma)
